34bf3924ab53c64315846992e78c341ee814d943
rendon.luisegerardo@gmail.com <LuisGerardoRendon>
2021-06-20 00:17:03 -0500

    Avanzando en el inicio de los códigos

---
+++ app/src/main/java/com/example/recycler/InicioCodigosDescuento.java
@@ -0,0 +1,18 @@
+package com.example.recycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class InicioCodigosDescuento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_codigos_descuento);
+    }
+
+    public void clickPerfil(View view) {
+    }
+}
