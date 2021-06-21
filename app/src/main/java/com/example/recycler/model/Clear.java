a76dcd5bb46cdab0af6bead1e027312b2b3c20d2
rendon.luisegerardo@gmail.com <LuisGerardoRendon>
2021-06-21 02:28:26 -0500

    Se compuso el picaso

---
+++ app/src/main/java/com/example/recycler/model/Clear.java
@@ -0,0 +1,9 @@
+package com.example.recycler.model;
+
+import com.squareup.picasso.Picasso;
+
+public class Clear {
+    public static void clearCache (Picasso p) {
+        //p.cache.clear();
+    }
+}
